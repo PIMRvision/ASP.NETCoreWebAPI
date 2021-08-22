467e2a40a8d6995dcc7e9d5210f80e78106abf00
PIMRvision <88776550+PIMRvision@users.noreply.github.com>
2021-08-22 17:44:57 -0300

    31. [ESTUDO] HATEOAS - Links - Self

--- Seção 04/01_Fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
+++ Seção 04/01_Fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MimicAPI.Helpers;
 using MimicAPI.Models;
+using MimicAPI.Models.DTO;
 using MimicAPI.Repositories.Contracts;
 using Newtonsoft.Json;
 using System;
@@ -14,17 +16,19 @@ namespace MimicAPI.Controllers
     [Route("api/palavras")]
     public class PalavrasController : ControllerBase
     {
-        private readonly IPalavraRepository  _repository;
+        private readonly IPalavraRepository _repository;
+        private readonly IMapper _mapper;
 
-        public PalavrasController(IPalavraRepository repository)
+        public PalavrasController(IPalavraRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         //APP -- /api/palavras
         [Route("")]
         [HttpGet]
-        public ActionResult ObterTodas([FromQuery]PalavraUrlQuery query)
+        public ActionResult ObterTodas([FromQuery] PalavraUrlQuery query)
         {
             var item = _repository.ObterPalavras(query);
 
@@ -47,13 +51,21 @@ namespace MimicAPI.Controllers
             if (obj == null)
                 return NotFound();
 
-            return Ok(obj);
+            PalavraDTO palavraDTO = _mapper.Map<Palavra, PalavraDTO>(obj);
+
+            palavraDTO.Links = new List<LinkDTO>();
+            palavraDTO.Links.Add(
+                new LinkDTO("self", $"https://localhost:44367/api/palavras/{palavraDTO.Id}", "Get")
+            );
+
+
+            return Ok(palavraDTO);
         }
 
         // -- /api/palavras (POST: id, nome, ativo, pontuacao, criacao)
         [Route("")]
         [HttpPost]
-        public ActionResult Cadastrar([FromBody]Palavra palavra)
+        public ActionResult Cadastrar([FromBody] Palavra palavra)
         {
             _repository.Cadastrar(palavra);
             return Created($"/api/palavras/{palavra.Id}", palavra);
@@ -62,7 +74,7 @@ namespace MimicAPI.Controllers
         // -- /api/palavras/1 (PUT: id, nome, ativo, pontuacao, criacao)
         [Route("{id}")]
         [HttpPut]
-        public ActionResult Atualizar(int id, [FromBody]Palavra palavra)
+        public ActionResult Atualizar(int id, [FromBody] Palavra palavra)
         {
             var obj = _repository.Obter(id);
             if (obj == null)
@@ -84,7 +96,7 @@ namespace MimicAPI.Controllers
                 return NotFound();
 
             _repository.Deletar(id);
-           
+
             return NoContent();
         }
     }
