8f57bf79027d42c6647937e4ff198203e3b545a0
PIMRvision <88776550+PIMRvision@users.noreply.github.com>
2021-08-31 18:28:30 -0300

    49. [ESTUDO] UsuarioRepository

---
+++ Seção 05/01_Fim/MinhasTarefas/MinhasTarefas/Repositories/Contracts/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using MinhasTarefas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefas.Repositories.Contracts
+{
+    public interface IUsuarioRepository
+    {
+        void Cadastrar(ApplicationUser usuario, string senha);
+        ApplicationUser Obter(string email, string senha);
+    }
+}
