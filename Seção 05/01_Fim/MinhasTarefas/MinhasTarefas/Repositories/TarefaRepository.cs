d69c8b1760f850593e9b0f247519bb72459a7846
PIMRvision <88776550+PIMRvision@users.noreply.github.com>
2021-10-14 19:08:18 -0300

    50. [ESTUDO] TarefaRepository - Restauração - Parte 1

---
+++ Seção 05/01_Fim/MinhasTarefas/MinhasTarefas/Repositories/TarefaRepository.cs
@@ -0,0 +1,39 @@
+﻿using MinhasTarefas.Database;
+using MinhasTarefas.Models;
+using MinhasTarefas.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefas.Repositories
+{
+    public class TarefaRepository : ITarefaRepository
+    {
+        private readonly MinhasTarefasContext _banco;
+        public TarefaRepository(MinhasTarefasContext banco)
+        {
+            _banco = banco;
+        }
+
+        public List<Tarefa> Restauracao(ApplicationUser usuario, DateTime dataUltimaSincronizacao)
+        {
+            var query = _banco.Tarefas.Where(a=>a.UsuarioID == usuario.Id).AsQueryable();
+
+            if (dataUltimaSincronizacao == null)
+            {
+                query.Where(a => a.Criado >= dataUltimaSincronizacao || a.Atualizado >= dataUltimaSincronizacao);
+            }
+
+            return query.ToList<Tarefa>();
+        }
+
+        public void Sincronizacao(List<Tarefa> tarefas)
+        {
+            // Cadastrar novos registros
+
+            //Atualização de registro (Excluido)
+            throw new NotImplementedException();
+        }
+    }
+}
